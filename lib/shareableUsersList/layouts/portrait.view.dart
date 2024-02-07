e8948e2ffe2e842776e0c1fe32e41bcb636225fc
Mahmoud Nabhan <niypoo@gmail.com>
2024-02-07 10:26:40 +0200

    fix

--- lib/shareableUsersList/layouts/portrait.view.dart
+++ lib/shareableUsersList/layouts/portrait.view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:fly_ui/extensions/responsive.extension.dart';
 import 'package:fly_ui/views/layouts/nestedScrollView.widget.dart';
-import 'package:fly_ui/views/widgets/sliverAppBae.widget.dart';
+import 'package:fly_ui/views/widgets/appBar.widget.dart';
+import 'package:fly_ui/views/widgets/sliverContainer.widget.dart';
 import 'package:get/get.dart';
 import 'package:shareable_module/shareableUsersList/widgets/shareableUsersList.widget.dart';
 
@@ -17,8 +19,15 @@ class PortraitView extends StatelessWidget {
       // APP BAR
       headerSliverBuilder: [
         SliverPersistentHeader(
-          delegate: FlySliverAppBar(
-            title: 'Sharing'.tr,
+          pinned: true,
+          floating: false,
+          delegate: FlySliverContainer(
+            initMaxExtent: 53.sp,
+            initMinExtent: 45.sp,
+            child: (bool isCollapse, double shrinkOffset) => Container(
+              color: Get.theme.scaffoldBackgroundColor,
+              child: FlyAppBar(title: 'Sharing'.tr),
+            ),
           ),
         ),
       ],
--- lib/shareableUsersList/shareableUsersList.view.dart
+++ lib/shareableUsersList/shareableUsersList.view.dart
@@ -11,8 +11,9 @@ class ShareableUsersListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const FlyScaffold(
-        child: FlyResponsiveView(
-      child: PortraitView(),
-    ));
+      child: FlyResponsiveView(
+        child: PortraitView(),
+      ),
+    );
   }
 }
