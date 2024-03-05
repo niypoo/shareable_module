7f740f3ce697bb7b694076b1765f91b30850fd14
Mahmoud Nabhan <niypoo@gmail.com>
2024-03-05 09:13:56 +0200

    changes

--- lib/shareableUsersList/layouts/portrait.view.dart
+++ lib/shareableUsersList/layouts/portrait.view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fly_ui/extensions/responsive.extension.dart';
 import 'package:fly_ui/modules/searchInput/searchInput.widget.dart';
 import 'package:fly_ui/views/layouts/nestedScrollView.widget.dart';
-import 'package:fly_ui/views/widgets/appBar.widget.dart';
+import 'package:fly_ui/views/widgets/customAppBar.widget.dart';
 import 'package:fly_ui/views/widgets/sliverContainer.widget.dart';
 import 'package:get/get.dart';
 import 'package:shareable_module/shareableUsersList/shareableUsersList.controller.dart';
@@ -21,17 +21,16 @@ class PortraitView extends GetView<ShareableUsersListController> {
       // APP BAR
       headerSliverBuilder: [
         SliverPersistentHeader(
-          pinned: true,
-          floating: false,
           delegate: FlySliverContainer(
-            initMaxExtent: 53.sp,
-            initMinExtent: 45.sp,
-            child: (bool isCollapse, double shrinkOffset) => Container(
-              color: Get.theme.scaffoldBackgroundColor,
-              child: FlyAppBar(title: 'Sharing'.tr),
-            ),
+            initMaxExtent: 50,
+            initMinExtent: 50,
+            child: (bool isCollapse, double shrinkOffset) =>
+                 FlyCustomAppBar(title: 'Sharing'.tr),
           ),
+          pinned: false,
+          floating: false,
         ),
+        
         SliverPersistentHeader(
           pinned: true,
           floating: false,
