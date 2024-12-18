714c4a5b025a52a6c597e3a7e9a889067f310952
Mahmoud Nabhan <niypoo@gmail.com>
2024-12-18 17:46:00 +0200

    Update shareUser.model.dart

--- lib/models/shareUser.model.dart
+++ lib/models/shareUser.model.dart
@@ -89,7 +89,7 @@ class ShareUser implements BaseUser {
             : DateTime.now().toIso8601String(),
       };
 
-  static List<ShareUser> fromMap(List<dynamic>? data) {
+  static List<ShareUser> fromMap(Map<String, dynamic>? data) {
     // define list t return back
     final List<ShareUser> payloadList = [];
 
@@ -97,7 +97,7 @@ class ShareUser implements BaseUser {
     if (data == null || data.isEmpty) return payloadList;
 
     // iterable
-    for (final friend in data) {
+    for (final friend in data.values) {
       payloadList.add(ShareUser.fromData(friend));
     }
 
